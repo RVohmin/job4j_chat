f574af80e53079ead39e372fd74e087913c6605b
RVohmin <vohmin77@gmail.com>
2020-10-08 15:34:13 +0300

    2. Создания чата на Rest API. [#304583]

---
+++ src/main/java/ru/job4j/chat/ChatApplication.java
@@ -0,0 +1,23 @@
+package ru.job4j.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class ChatApplication {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+//    private static void getPass() {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String pwd = encoder.encode("password");
+//        System.out.printf("Password for 'password': %s", pwd);
+//    }
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}
